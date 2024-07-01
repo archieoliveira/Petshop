e7d5334507e630daaec8be42157681f73745f352
Artur Oliveira <arturolivv@gmail.com>
2024-07-01 19:47:41 -0300

    first commit

---
+++ Dog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace Petshop
+{
+    public class Dog
+    {
+        public double Weight { get; set; }
+        public string Fur { get; set; }
+        public double ExtraValue { get; set; } = 0; 
+
+        public Dog()
+        {
+        }
+
+        public Dog(double weight, string fur)
+        {
+            Weight = weight;
+            Fur = fur;
+        }
+
+        public Dog(double weight, string fur, double extraValue) : this(weight, fur)
+        {
+            ExtraValue = extraValue;
+        }
+
+        public double Calculate()
+        {
+            double baseValue = 0;
+            double multiplier;
+            double finalValue;
+
+            if (Weight < 3)
+            {
+                baseValue = 40.00;
+            }
+            else if (Weight < 10)
+            {
+                baseValue = 50.00;
+            }
+            else if (Weight >= 10 && Weight < 30)
+            {
+                baseValue = 60.00;
+            }
+            else
+            {
+                baseValue = 70.00;
+            }
+
+            if (Fur == "s" || Fur == "S")
+            {
+                multiplier = 1.0;
+            }
+            else if (Fur == "m" || Fur == "M")
+            {
+                multiplier = 1.5;
+            }
+            else
+            {
+                multiplier = 2.0;
+            }
+
+            finalValue = (baseValue * multiplier) + ExtraValue;
+
+            return finalValue;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace Petshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog();
+
+            Console.WriteLine("------------------ Define the dog's weight ------------------");
+            do
+            {
+                Console.Write(">> ");
+                dog1.Weight = double.Parse(Console.ReadLine());
+
+                if (dog1.Weight > 0 && dog1.Weight <= 50)
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("It's only accepted weights between 0kg and 50kg");
+                    Console.WriteLine();
+                }
+            } while (true);
+
+
+            Console.WriteLine("------------------- Define the fur length -------------------");
+            Console.WriteLine("------------- S = Short | M = Medium | L = Long -------------");
+            do
+            {
+                Console.Write(">> ");
+                dog1.Fur = Console.ReadLine().ToUpper();
+
+                if (dog1.Fur == "S" || dog1.Fur == "M" || dog1.Fur == "L")
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Entry only the valid options: S/M/L");
+                    Console.WriteLine();
+                }
+            } while (true);
+
+            Console.WriteLine("------------------ Choose extra services ------------------");
+            Console.WriteLine("Enter the option (1) to cut the nails - R$10.00");
+            Console.WriteLine("Enter the option (2) to brush the teeth - R$12.00");
+            Console.WriteLine("Enter the option (3) to clean the dog ear - R$15.00");
+            Console.WriteLine("Enter the option (0) if you do not want an additional service");
+
+            string extra;
+            do
+            {
+                Console.Write(">> ");
+                extra = Console.ReadLine();
+
+                switch (extra)
+                {
+                    case "1":
+                        dog1.ExtraValue += 10.00;
+                        break;
+                    case "2":
+                        dog1.ExtraValue += 12.00;
+                        break;
+                    case "3":
+                        dog1.ExtraValue += 15.00;
+                        break;
+                }
+            } while (extra != "0");
+
+            Console.WriteLine();
+            Console.WriteLine($"Value to pay: ${dog1.Calculate().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
